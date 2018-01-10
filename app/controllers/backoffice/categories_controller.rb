47956fef6dd91a5e4326fb8d56488e7ae7fdf3bd
jonathan <jonathannascimento70@gmail.com>
2018-01-10 00:30:04 -0300

    novas rotas

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,11 +1,27 @@
 class Backoffice::CategoriesController < BaseController
-
   def index
     @categories = Category.all
   end
 
-  def edit
+  def edit; end
 
+  def create
+    @category = Category.new(params_category)
+
+    if @category.save
+      redirect_to backoffice_categories_path, notice: 'A Categoria foi salva'
+    else
+      render :new
+    end
   end
 
+  def new
+    @category = Category.new
+  end
+
+  private
+
+  def params_category
+    params.require(:category).permit(:description)
+  end
 end
